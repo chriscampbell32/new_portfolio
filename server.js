fc872c2bf7263ffb602a2c3b8f2e76d27f36e618
chriscampbell32 <chriscampbell32@yahoo.com>
2016-04-12 18:32:04 -0400

    working properly on local server

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-//serve it up serve it up
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 8080;
+
+
+app.use(express.static("public"));
+app.use(express.static("views"));
+
+//routes 
+app.get('/', function(req,res){
+    res.sendFile(process.cwd() + '/views/homepage.html');
+});
 
 
 //PORT listening
