6e85dbc2d22980fe46357402348a56feb865c75c
chriscampbell32 <chriscampbell32@yahoo.com>
2016-04-12 16:38:31 -0400

    server is up and listening

--- server.js
+++ server.js
@@ -4,3 +4,8 @@ var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+
+//PORT listening
+app.listen(PORT, function(req,res){
+    console.log('server is listening on port %s', PORT)
+});
